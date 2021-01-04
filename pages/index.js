ac9a7691d0a2265f5ff9edabc4a9428684688b2b
Donte Dwight <ddwight@clemson.edu>
2021-01-04 13:11:25 -0500

    commit: updating index

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <Game />
+      <footer className="text-sm text-center font-bold">&copy; Copyright {2021}, Dont'e Dwight. All Rights Reserved</footer>
     </div>
   )
 }
